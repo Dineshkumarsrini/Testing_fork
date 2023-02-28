e7cd6b7cae781c42829d9fd90707d413ae9cbe5d
Dineshkumarsrini <107687653+Dineshkumarsrini@users.noreply.github.com>
2023-02-28 19:41:57 +0530

    revert the lancher in demoActivity

--- app/src/main/java/com/example/myapplication/DemoActivity.kt
+++ app/src/main/java/com/example/myapplication/DemoActivity.kt
@@ -1,10 +1,9 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.myapplication.R
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_demo.*
 
 class DemoActivity : AppCompatActivity() {
@@ -12,22 +11,6 @@ class DemoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
         val intent : Intent = Intent(this@DemoActivity,com.acuant.sampleapp.MainActivity::class.java)
-        acuantCaptureIntent.launch(intent)
+        startActivity(intent)
     }
-
-    val acuantCaptureIntent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
-        val data: Intent? = result.data
-        when (result.resultCode) {
-            RESULT_OK -> {
-                tv_hello_world.text = data?.getStringExtra("RETURN_TEXT").toString()
-            }
-            RESULT_CANCELED -> {
-                tv_hello_world.text = "fail"
-            }
-            else -> {
-                tv_hello_world.text = "fail"
-            }
-        }
-    }
-
 }
