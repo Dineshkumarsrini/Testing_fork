d3539dcd6813962fc3e95edb539197da985f467e
Dineshkumarsrini <107687653+Dineshkumarsrini@users.noreply.github.com>
2023-02-28 12:30:33 +0530

    update the changes for roaming instance

--- app/src/main/java/com/example/myapplication/DemoActivity.kt
+++ app/src/main/java/com/example/myapplication/DemoActivity.kt
@@ -3,13 +3,31 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.testing.R
+import com.example.myapplication.R
+import androidx.activity.result.contract.ActivityResultContracts
+import kotlinx.android.synthetic.main.activity_demo.*
 
 class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
         val intent : Intent = Intent(this@DemoActivity,com.acuant.sampleapp.MainActivity::class.java)
-        startActivity(intent)
+        acuantCaptureIntent.launch(intent)
     }
+
+    val acuantCaptureIntent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        val data: Intent? = result.data
+        when (result.resultCode) {
+            RESULT_OK -> {
+                tv_hello_world.text = data?.getStringExtra("RETURN_TEXT").toString()
+            }
+            RESULT_CANCELED -> {
+                tv_hello_world.text = "fail"
+            }
+            else -> {
+                tv_hello_world.text = "fail"
+            }
+        }
+    }
+
 }
